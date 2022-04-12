d07c2d5dc90152081caa6d1cc8a91fa887ad9e7f
Boris Stankov <boris.stankovv@gmail.com>
2022-04-12 21:48:03 +0300

    adding to the repo

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import NewCity from './components/NewCity.vue';
+</script>
+
+<template>
+<div>
+  <div id="head">
+  <h1>Data for the bigger cities is preloaded</h1>
+  <h3> you can also add some of your own via the form below:</h3>
+  </div>
+  <form class="form" @submit.prevent="onSubmit" @submit="addCity">
+    <input id="input" type="text" v-model="myText" placeholder="Type a city">
+    <button id="button"  >Submit</button>
+  </form>
+ <NewCity :key="index" v-for="(city, index) in this.cityArray" :city="city"/>
+</div>
+</template>
+
+
+<script>
+export default{
+  name:"app",
+  components:{
+  NewCity,
+  },
+  data(){
+    return{
+      cityArray:["sofia","varna","burgas","plovdiv"]
+    }
+  },
+  methods:{
+    addCity(){
+      this.cityArray.push(this.myText)
+      this.myText = ''
+    }
+  }
+}
+</script>
+<style>
+@import './assets/base.css';
+</style>
---
+++ src/components/NewCity.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="city">
+        <div>
+        <h1>{{dataArray?.name}}</h1>
+        <h2>{{dataArray?.main.temp.toFixed(1)}} &#8451;</h2>
+        </div>
+        <img style="height:70px" :src='(` http://openweathermap.org/img/wn/${dataArray?.weather[0].icon}.png`)' alt="">
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default{
+    name:'form',
+   async created(){
+      await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`)
+       .then(res =>{
+          
+           this.dataArray = res.data
+           console.log(this.dataArray)
+       })
+    },
+data(){
+    return{
+    apiKey : "a6f922dcdd663a60627025e5586baeff",
+    dataArray:null,
+    }
+},
+props:{
+    city:String,
+   
+},
+methods:{
+}
+}
+</script>
+
