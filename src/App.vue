6fde9e537dc7fe8f56376e143fcaa72c0c76dc0d
Boris Stankov <103064087+BorisStankov98@users.noreply.github.com>
2022-04-13 10:46:41 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import NewCity from './components/NewCity.vue';
     <button id="button"  >Submit</button>
   </form>
    <NewCity :city="Varna"/>
-   <NewCity :key="index" v-for="(city, index) in this.cityArray?" :city="city"/>
+   <NewCity :key="index" v-for="(city, index) in this.cityArray" :city="city"/>
 </div>
 </template>
 
