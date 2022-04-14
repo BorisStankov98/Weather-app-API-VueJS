78e88830773336351feb5d347254375e56b4d2c6
Boris Stankov <boris.stankovv@gmail.com>
2022-04-14 19:09:58 +0300

    Update NewCity.vue

--- src/components/NewCity.vue
+++ src/components/NewCity.vue
@@ -19,7 +19,7 @@ export default{
     },
     data() {
         return {
-            innitialArray:["sofia","varna","burgas","plovdiv","london"],
+            innitialArray:["sofia","varna","burgas","plovdiv",],
             apiKey: "a6f922dcdd663a60627025e5586baeff",
             dataArray: []
         };
@@ -28,6 +28,7 @@ export default{
         city: String,
     },
     methods: {
+        //on page load renders all pre-decided items
        async callApi(place){
        this.innitialArray.slice(place).forEach((element) => {
        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${element}&appid=${this.apiKey}&units=metric`)
@@ -37,9 +38,11 @@ export default{
           alert(`${this.myText}is not in the database please try another one :)`)
         })   
         })},
+        //adds new item and passes the end of the array as the argument for the forEach
       addCity(){
       this.innitialArray.push(this.myText)
       this.callApi(this.innitialArray.length-1)
+      // still trying to figure out how to resset the input value this.Mytext = '' resets only the backend value
        }
     },
     components: { City }
